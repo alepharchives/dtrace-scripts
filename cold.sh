7a66837ab5a29bcf026c6298868d08fcafa9fcb2
wagerlabs <joelr1@gmail.com>
2009-08-28 09:24:50 +0100

    allow several dtrace scripts

--- cold.sh
+++ cold.sh
@@ -1,6 +1,9 @@
 #!/bin/bash 
 
 cmd="./Minefield.app/Contents/MacOS/firefox-bin -no-remote -foreground -P 2"
+scripts=""
 
-sync && purge && dtrace -x dynvarsize=64m -x evaltime=exec -c "$cmd" -wZs $1
+for i in $*; do scripts="$scripts -s $i"; done
+
+sync && purge && dtrace -x dynvarsize=64m -x evaltime=exec -c "$cmd" -wZ $scripts
 
