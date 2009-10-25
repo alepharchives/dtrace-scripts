6bc117cf5f962a89bf81ff6ac5c6d50912e26ae4
wagerlabs <joelr1@gmail.com>
2009-10-25 20:22:02 +0000

    more or less working

--- lib.d
+++ lib.d
@@ -1,86 +1,43 @@
-/*
- * We will discard whatever we captured unless
- * we have a dlopen -> open -> mmap call chain.
- */  
-
-BEGIN
-{
-  d = 0; /* depth */
-}
-
 pid$target::dlopen:entry
 /arg0/
 {
-  printf("opening %s\n", copyinstr(arg0));
+  self->path = copyinstr(arg0);
 }
 
-pid$target::dlopen:entry
-/arg0/
+pid$target::dlopen:return
+/self->path != 0/
 {
-  self->path[d] = copyinstr(arg0);
-  self->spec[d] = speculation();
-  d++;
-  speculate(self->spec[d]);
-
-  printf("\n\n");
-  printf("dlopen(\"%s\", ...)\n", self->path[d]);
+  self->path = 0;
 }
+
+/* opening the same file */
 
 pid$target::open:entry
-/self->spec[d] && self->path[d] != 0 && self->path[d] == copyinstr(arg0)/
-{ 
-  speculate(self->spec[d]);
-  self->savefd[d] = 1;
+/self->path != 0 && self->path == copyinstr(arg0)/
+{
+  self->savefd = 1;
 }
+
+/* save file descriptor of a dlopen-ed library */
 
 pid$target::open:return
-/self->spec[d] && self->savefd[d]/
+/self->savefd/
 {
-  speculate(self->spec[d]);
-  self->fd[d] = arg1;
-  printf("  -> open(\"%s\", ...) = %d\n", self->path[d], self->fd[d]);
+  self->fd[arg1] = self->path;
+  self->savefd = 0;
 }
+
+/* mmap-ing a library we know about */
 
 pid$target::mmap:entry
-/self->spec[d] && self->fd[d] && self->fd[d] == arg4/
+/self->fd[arg4] != 0/
 {
-  speculate(self->spec[d]);
-  self->mmap_addr[d] = arg0;
-  self->mmap_len[d] = arg1;
-  self->mmap_fd[d] = arg4;
-  self->mmap_offset[d] = arg5;
-  printf("   -> mmap(%x, %u, ..., ..., %d, %u)\n", 
-    self->mmap_addr[d], self->mmap_len[d], 
-    self->mmap_fd[d], self->mmap_offset[d]);
+  self->mmap_addr = arg0;
+  self->mmap_len = arg1;
+  self->mmap_fd = arg4;
+  self->mmap_offset = arg5;
+  this->path = self->fd[arg4];
+  
+  printf("mapping %u bytes of %s from offset %d to address 0x%x\n\n",
+    self->mmap_len, this->path, self->mmap_offset, self->mmap_addr);
 }
-
-pid$target::dlopen:return
-/self->spec[d] && self->fd[d]/
-{
-  commit(self->spec[d]);
-  self->spec[d] = 0;
-}
-
-pid$target::dlopen:return
-/self->spec[d]/
-{
-  discard(self->spec[d]);
-  self->spec[d] = 0;
-}
-
-pid$target::dlopen:return
-{
-  self->path[d] = 0;
-  self->savefd[d] = 0;
-  self->fd[d] = 0;
-}
-
-pid$target::dlopen:return
-/d > 0/
-{
-  d--;
-}
-
-
-
-
