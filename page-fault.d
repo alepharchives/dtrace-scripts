13ceaa39ccfba191cf0250b5b74faffeaa3d3e76
wagerlabs <joelr1@gmail.com>
2009-10-28 16:29:53 +0000

    working now

--- page-fault.d
+++ page-fault.d
@@ -14,6 +14,7 @@ pid$target::dlopen:return
 }
 
 pid$target::dyld??loadPhase5*:entry
+/arg0/
 {
   self->path = copyinstr(arg0);
 }
@@ -41,14 +42,6 @@ pid$target::open:entry
 
 /* opening the same file */
 
-/*
-pid$target::open:entry
-/self->path != 0 && self->path == copyinstr(arg0)/
-{
-  self->savefd = 1;
-}
-*/
-
 fbt::vn_open_auth:entry
 {
   this->ndp = arg0;
@@ -57,14 +50,22 @@ fbt::vn_open_auth:entry
 fbt::vn_open_auth:return
 /self->path == ((nameidata_t)this->ndp)->ni_pathbuf/
 {
-  printf("Got a match on %s\n", self->path);
   this->vp = (vnode_t)((nameidata_t)this->ndp)->ni_vp;
   this->lib = stringof((this->vp)->v_name);
   self->lib[this->lib] = self->path;
+  self->vnode[this->lib] = this->vp; /* unused */
 }
 
 fbt::vnode_pagein:entry
-/self->lib[stringof(((vnode_t)arg0)->v_name)] != 0/
 {
-  printf("vnode_pagein(vnode ptr: %x = %s, upl: %x, upl_offset: %u, f_offset: %u, size: %u, flags: %d, ...)\n", arg0, self->vnode[(vnode_t)arg0], arg1, arg2, arg3, arg4, arg5);
+  self->v_name = stringof(((vnode_t)arg0)->v_name);
+}
+
+/* vnode pointers match but v_name seems more secure */
+
+fbt::vnode_pagein:entry
+/self->lib[self->v_name] != 0/
+{
+  printf("vnode_pagein: %s, offset: %u, size: %u\n", 
+    self->v_name, arg3, arg4);
 }
