5b323a6037c744eff55a0fe162355ba7f5551bfd
wagerlabs <joelr1@gmail.com>
2010-01-21 10:34:19 +0000

    linux fix

--- linux-vlad.sh
+++ linux-vlad.sh
@@ -5,7 +5,7 @@ TIME=$NAME.csv
 
 
 function run {
-  ./purge.sh
+  ./linux-purge.sh
   /home/moco/Work/$1/dist/firefox/firefox -no-remote -P clean file:///home/moco/Work/firefox-startup/startup.html#`python -c 'import time; print int(time.time() * 1000);'`
 }
 
